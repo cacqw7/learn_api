a419e2cf968ea8818ce778f82a8e99974563abb8
Chase Clettenberg <clettenberg@gmail.com>
2015-10-07 23:01:01 -0500

    [skip] Added seeds (2m)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+links = Link.create([
+  {
+    title: "Lotus 0.5.0",
+    url: "http://lotusrb.org/blog/2015/09/30/announcing-lotus-050.html",
+    description: "They've just put out another release of the Lotus Framework. Could be interesting."
+  },
+  {
+    title: "Rails for Zombies",
+    url: "http://railsforzombies.org/",
+    description: "Could be useful if any of your friends are looking to get into the Rails game."
+  }
+])
