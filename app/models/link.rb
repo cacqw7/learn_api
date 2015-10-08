ccc5e685ec62eb26e39b3aafabe44f9b79ad1057
Chase Clettenberg <clettenberg@gmail.com>
2015-10-07 23:01:19 -0500

    [skip] Saved link thumbnail url (10m)

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,19 @@
 class Link < ActiveRecord::Base
+  before_save :save_link_thumbnail
+
+private
+  def save_link_thumbnail
+    #set the default image
+    self.thumbnail_url = "missing_img.jpg"
+
+    link = LinkThumbnailer.generate(self.url)
+    return unless url = link.images.first.src.to_s
+    self.thumbnail_url = url if link_is_valid?(url)
+  end
+
+  def link_is_valid?(url)
+    bad_codes = [403, 404]
+    response = HTTParty.get(url)
+    !bad_codes.include?(response.code)
+  end
 end
